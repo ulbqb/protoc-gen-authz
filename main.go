3055d62e6dfcb12505af6d100b3bb51ab539af14
Hyodo <mmoshg8u@gmail.com>
2022-06-26 19:11:33 +0900

    Add authz algorithm

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
-package hello
+package main
 
-func Hello() string {
-    return "Hello"
+import (
+	pgs "github.com/lyft/protoc-gen-star"
+	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
+	"github.com/ulbqb/protoc-gen-authz/module"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	optional := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	pgs.Init(
+		pgs.DebugEnv("DEBUG"),
+		pgs.SupportedFeatures(&optional),
+	).RegisterModule(
+		module.Authz(),
+	).RegisterPostProcessor(
+		pgsgo.GoFmt(),
+	).Render()
 }
